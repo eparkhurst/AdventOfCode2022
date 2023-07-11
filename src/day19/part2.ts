8033967c670e427bb409d753cdf4c5a781a2972d
Elijah Parkhurst <eli@bouncecon.com>
2023-07-11 13:48:41 -0600

    doesnt work. I don't know why. fuck it

--- src/day19/part2.ts
+++ src/day19/part2.ts
@@ -3,147 +3,161 @@ const path = require('path');
 
 fs.readFile(path.resolve(__dirname, './data.txt'), 'utf-8', (err: any, data: any) => {
     if (err) throw err;
-    const lines = data
-        .trim()
-        .split('\n')
-        .map((line: string) => {
-            return line.split(',').map((el: string) => parseInt(el, 10));
-        });
+    const lines = data.trim().split('\n');
 
-    run(lines);
+    run(format(lines));
 });
-const dict: any = {};
-const getSurfaceArea = (lines: number[][]) => {
-    const particles = Array(lines.length).fill(6);
-    for (let i = 0; i < lines.length; i++) {
-        const [x, y, z] = lines[i];
-        if (dict[`${x},${y},${z}`]) {
-            console.log('duplicate');
-        } else {
-            dict[`${x},${y},${z}`] = true;
-        }
-        for (let j = i + 1; j < lines.length; j++) {
-            const otherEl = lines[j];
-            let contacts = 0;
-            let diff;
-            if (x === otherEl[0]) contacts++;
-            else diff = Math.abs(x - otherEl[0]);
-            if (y === otherEl[1]) contacts++;
-            else diff = Math.abs(y - otherEl[1]);
-            if (z === otherEl[2]) contacts++;
-            else diff = Math.abs(z - otherEl[2]);
-            if (contacts === 2 && diff === 1) {
-                particles[i]--;
-                particles[j]--;
-            }
-        }
-    }
-    const surfaceArea = particles.reduce((acc: number, el: number) => acc + el, 0);
-    return surfaceArea;
+
+type Resources = {
+    ore: number;
+    clay: number;
+    obsidian: number;
+    geode: number;
+};
+type Robot = 'ore' | 'clay' | 'obsidian' | 'geode';
+const format = (lines: string[]) => {
+    return lines.map((line: string) => {
+        const rules = line.split('.').filter((el: string) => !!el);
+        let bluePrint: any = {};
+        rules.forEach((rule: string) => {
+            const [_, logic] = rule.split('Each ');
+            const [type, cost] = logic.split(' robot costs ');
+            bluePrint[type] = {};
+            const kinds = cost.split(' and ');
+            kinds.forEach((kind: string) => {
+                const [amount, resource] = kind.split(' ');
+                bluePrint[type][resource] = parseInt(amount, 10);
+            });
+        });
+        return bluePrint;
+    });
 };
 
-const run = (lines: number[][]) => {
-    const map: string[][][] = [];
-    const voids: number[][] = [];
-    let xMax = -Infinity;
-    let yMax = -Infinity;
-    let zMax = -Infinity;
-    for (let i = 0; i < lines.length; i++) {
-        const [x, y, z] = lines[i];
-        if (x > xMax) xMax = x;
-        if (y > yMax) yMax = y;
-        if (z > zMax) zMax = z;
+const recur = (
+    bluePrint: any,
+    max: Resources,
+    resources: Resources,
+    robots: Resources,
+    time: number,
+    memo: any,
+): number => {
+    // if (count < 40) {
+    //     console.log(time, resources, robots);
+    //     count++;
+    // }
+    if (time === 0) {
+        return resources.geode;
     }
 
-    for (let i = 0; i < xMax + 1; i++) {
-        map.push([]);
-        for (let j = 0; j < yMax + 1; j++) {
-            //@ts-ignore
-            map[i].push([]);
-            for (let k = 0; k < zMax + 1; k++) {
-                //@ts-ignore
-                map[i][j].push('.');
+    const key = `${time}-${Object.values(robots).join('')}${Object.values(resources).join('')}`;
+
+    if (memo[key]) {
+        return memo[key];
+    }
+    //99lkjdshfoiuahsdfipjnsdf
+
+    let maxGeodes = resources.geode + robots.geode * time;
+    const robotArr = Object.keys(bluePrint) as Robot[];
+    for (let i = 0; i < robotArr.length; i++) {
+        const robot = robotArr[i] as Robot;
+        if (robot !== 'geode' && robots[robot] >= max[robot]) continue;
+
+        let delay = 0;
+        const robotCost: Resources = bluePrint[robot];
+        let skip = false;
+        const costArr = Object.keys(robotCost);
+        for (let j = 0; j < costArr.length; j++) {
+            const resource = costArr[j] as Robot;
+            if (robots[resource] === 0) {
+                skip = true;
+                break;
             }
+            if (resources[resource] >= robotCost[resource]) continue;
+            const possibleDelay = Math.floor((robotCost[resource] - resources[resource]) / robots[resource]);
+            if (possibleDelay < 0) {
+                console.log('whoa');
+            }
+            if (possibleDelay > delay) {
+                delay = possibleDelay;
+            }
+        }
+        if (skip) continue;
+
+        const timeLeft = time - delay - 1;
+        if (timeLeft <= 0) continue;
+
+        const nextRobots = { ...robots };
+        // update amount of resources
+        const nextResources = robotArr.reduce((next: any, type: Robot) => {
+            next[type] = resources[type] + robots[type] * (delay + 1);
+            return next;
+        }, {});
+
+        for (const type in robotCost) {
+            //@ts-ignore
+            nextResources[type] -= robotCost[type];
+        }
+        nextRobots[robot] += 1;
+        const geodes = recur(bluePrint, max, nextResources, nextRobots, timeLeft, memo);
+        if (geodes > maxGeodes) {
+            maxGeodes = geodes;
         }
     }
+    memo[key] = maxGeodes;
+    return maxGeodes;
+};
 
-    const checkInterior = (x: number, y: number, z: number): number => {
-        if (map[x]?.[y]?.[z] === undefined) return 0;
-        if (map[x]?.[y]?.[z] === '#') return 0;
-        let zInterior = false;
-        let yInterior = false;
-        let yInteriorD = false;
-        let xInterior = false;
-        let xInteriorD = false;
-        for (let i = z; i < zMax + 1; i++) {
-            //@ts-ignore
-            if (map[x][y][i] === '#') {
-                zInterior = true;
-                break;
-            }
-        }
-        for (let j = y; j <= yMax; j++) {
-            //@ts-ignore
-            if (map[x][j]?.[z] === '#') {
-                yInterior = true;
-                break;
-            }
-        }
-        for (let k = 0; k <= y; k++) {
-            //@ts-ignore
-            if (map[x][k]?.[z] === '#') {
-                yInteriorD = true;
-                break;
-            }
-        }
-        for (let j = x; j <= xMax; j++) {
-            //@ts-ignore
-            if (map[j][y]?.[z] === '#') {
-                xInterior = true;
-                break;
-            }
-        }
-        for (let k = 0; k <= x; k++) {
-            //@ts-ignore
-            if (map[k][y]?.[z] === '#') {
-                xInteriorD = true;
-                break;
-            }
-        }
-        if (zInterior && yInterior && yInteriorD && xInterior && xInteriorD) {
-            voids.push([x, y, z]);
-            return 1 + checkInterior(x, y, z + 1);
-        }
-        return 0;
+const runBluePrint = (bluePrint: any) => {
+    const resources: Resources = {
+        ore: 0,
+        clay: 0,
+        obsidian: 0,
+        geode: 0,
     };
-
-    for (let i = 0; i < lines.length; i++) {
-        const [x, y, z] = lines[i];
-        //@ts-ignore
-        map[x][y][z] = '#';
-    }
-    for (let i = 0; i < map.length; i++) {
-        for (let j = 0; j < map[i].length; j++) {
-            for (let k = 0; k < map[i][j].length; k++) {
-                if (map[i][j][k] === '#') {
-                    if (map[i][j][k + 1] === '.') {
-                        checkInterior(i, j, k + 1);
-                    }
+    const bots: Resources = {
+        ore: 1,
+        clay: 0,
+        obsidian: 0,
+        geode: 0,
+    };
+    const max: Resources = Object.values(bluePrint).reduce(
+        (max: Resources, robot: any) => {
+            for (const resource in robot) {
+                // @ts-ignore
+                if (robot[resource] > max[resource]) {
+                    // @ts-ignore
+                    max[resource] = robot[resource];
                 }
             }
-        }
-    }
-
-    const surfaceArea = getSurfaceArea(lines);
-    console.log(surfaceArea);
-    const negative = getSurfaceArea(voids);
-    console.log('negative surface area', negative);
-    console.log(surfaceArea - negative);
+            return max;
+        },
+        { ore: 0, clay: 0, obsidian: 0, geode: 0 },
+    );
+    console.log('max', max);
+    const memo: any = {};
+    const geode = recur(bluePrint, max, resources, bots, 24, memo);
+    return geode;
 };
 
-//last asnwser: 4482
-// too low 2400
-// too low 2546
-// 4164 too high
-// 2576 correct
+// const run = (blueprints: any[]) => {
+//     console.log('blueprints', blueprints[0]);
+//     let final = 1;
+//     for (let i = 0; i < 2; i++) {
+//         const geode = runBluePrint(blueprints[i]);
+//         console.log('geode', geode);
+//         final = final * geode;
+//     }
+//     console.log(final);
+// };
+const run = (bluePrints: any[]) => {
+    console.log('blueprints', bluePrints[0]);
+    const geode = runBluePrint(bluePrints[0]);
+    console.log('geode', geode);
+    // const final = blueprints.reduce((max: number, bluePrint: any, i: number) => {
+    //     max += (i + 1) * geode;
+    //     return max;
+    // }, 0);
+    // console.log(final);
+};
+
 export {};
